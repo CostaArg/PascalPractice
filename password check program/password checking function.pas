2adf87b1793b1363268b6c3faff5c694fcdc87ae
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-06-26 20:38:02 +0300

    added password check program

---
+++ password check program/password checking function.pas
@@ -0,0 +1,44 @@
+﻿Program PasswordTest;
+
+Const
+	saved='1234';
+
+Var
+	bool:boolean;
+	
+	Function checkpassword():boolean;
+		Var
+			password:string;
+			tries:integer;
+			check:boolean;
+
+	Begin
+		tries:=0;
+		check:= false;
+
+		Repeat
+			Write ('Enter your password : ');
+			Readln (password);
+			tries:= (tries + 1);
+			If (password = saved) then
+				Begin
+					Write ('Success! ');
+					check := true;
+				End;
+			If (check = false) then
+				Begin
+					Writeln ('You have entered a wrong password, please try again');
+					Writeln ('The times you entered a password are : ', tries);
+				End;
+			If ( (tries=3) and (check = false) ) then
+				Writeln ('ACCESS DENIED!!!')
+		Until (  (password = saved) or (  (tries>=3) and (check = false)  )  );
+	checkpassword:=check;
+End;
+
+Begin
+bool:=checkpassword();	
+If (bool=true) then
+	writeln('You are now authorized!');
+End.
+
